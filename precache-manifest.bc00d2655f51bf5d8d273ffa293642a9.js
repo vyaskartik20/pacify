9c67084ebafec47f7f29176d7f6738d754647d60
vyaskartik20 <vyas.2@iitj.ac.in>
2020-08-06 02:50:19 +0530

    Updates

---
+++ precache-manifest.bc00d2655f51bf5d8d273ffa293642a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4550f3e7639bbb688484904124a9a40c",
+    "url": "/timepalace/index.html"
+  },
+  {
+    "revision": "54065ed3e98c12ae42b7",
+    "url": "/timepalace/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "ac3f4981c05c65b84f71",
+    "url": "/timepalace/static/css/main.44aaa5a3.chunk.css"
+  },
+  {
+    "revision": "54065ed3e98c12ae42b7",
+    "url": "/timepalace/static/js/2.405e2cc7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/timepalace/static/js/2.405e2cc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac3f4981c05c65b84f71",
+    "url": "/timepalace/static/js/main.0947eb8f.chunk.js"
+  },
+  {
+    "revision": "cff5b77c2e73359c4bd0",
+    "url": "/timepalace/static/js/runtime-main.655087fe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/timePalace/precache-manifest.8b06105e4756eac256c6350f01bebffa.js"
+  "/timepalace/precache-manifest.bc00d2655f51bf5d8d273ffa293642a9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timePalace/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timepalace/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
