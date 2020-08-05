e80e5f03daa2bb6dcbf7cb9a644f88969959fb8e
vyaskartik20 <vyas.2@iitj.ac.in>
2020-08-06 01:06:23 +0530

    Updates

---
+++ precache-manifest.947308bb8c61b974e6109d5b2777dee4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f109c9a92e9872d1d1e3d639404957b3",
+    "url": "/Time-Palace/index.html"
+  },
+  {
+    "revision": "1b33d786a627c613adbe",
+    "url": "/Time-Palace/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "7e17121c4608269c9866",
+    "url": "/Time-Palace/static/css/main.1079886d.chunk.css"
+  },
+  {
+    "revision": "1b33d786a627c613adbe",
+    "url": "/Time-Palace/static/js/2.679ca7d4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Time-Palace/static/js/2.679ca7d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e17121c4608269c9866",
+    "url": "/Time-Palace/static/js/main.7beb8d02.chunk.js"
+  },
+  {
+    "revision": "e691d159dcb48bf837a4",
+    "url": "/Time-Palace/static/js/runtime-main.74ca9dfc.js"
+  }
+]);
