812a53526dbfecd240974167687fd48be42f8c4a
vyaskartik20 <vyas.2@iitj.ac.in>
2020-08-12 04:34:25 +0530

    Updates

---
+++ precache-manifest.b082f9f3f02086ce115d564b5c032077.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10039d1d73c27012140770b215a079f1",
+    "url": "/timepalace/index.html"
+  },
+  {
+    "revision": "e12f43b79a5422c03b78",
+    "url": "/timepalace/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "0deb27da14b444b5cdd9",
+    "url": "/timepalace/static/css/main.2766bc65.chunk.css"
+  },
+  {
+    "revision": "e12f43b79a5422c03b78",
+    "url": "/timepalace/static/js/2.19ee6ef8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/timepalace/static/js/2.19ee6ef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0deb27da14b444b5cdd9",
+    "url": "/timepalace/static/js/main.8172de42.chunk.js"
+  },
+  {
+    "revision": "cff5b77c2e73359c4bd0",
+    "url": "/timepalace/static/js/runtime-main.655087fe.js"
+  },
+  {
+    "revision": "5afb17bad19ec982fcc5fb7e8289cd4c",
+    "url": "/timepalace/static/media/coding20.5afb17ba.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/timepalace/precache-manifest.bc00d2655f51bf5d8d273ffa293642a9.js"
+  "/timepalace/precache-manifest.b082f9f3f02086ce115d564b5c032077.js"
 );
 
 self.addEventListener('message', (event) => {
