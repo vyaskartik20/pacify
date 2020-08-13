452bff04030ff556b68621755518b2dfd5168111
vyaskartik20 <vyas.2@iitj.ac.in>
2020-08-13 22:06:38 +0530

    Updates

---
+++ precache-manifest.6f79be4faeaa9c2be31153c12bfd8a53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072cec79d4473fc0bed8b9657d15bc34",
+    "url": "/pacify/index.html"
+  },
+  {
+    "revision": "940375c556ab57575f31",
+    "url": "/pacify/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "0630079a8aa20109ea11",
+    "url": "/pacify/static/css/main.32be3857.chunk.css"
+  },
+  {
+    "revision": "940375c556ab57575f31",
+    "url": "/pacify/static/js/2.cd3bbe6c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pacify/static/js/2.cd3bbe6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0630079a8aa20109ea11",
+    "url": "/pacify/static/js/main.5cf74303.chunk.js"
+  },
+  {
+    "revision": "543063dbc1536e952212",
+    "url": "/pacify/static/js/runtime-main.399ecfbd.js"
+  },
+  {
+    "revision": "5afb17bad19ec982fcc5fb7e8289cd4c",
+    "url": "/pacify/static/media/coding20.5afb17ba.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/timepalace/precache-manifest.bd2630dddd819e28ea6c40a16e853072.js"
+  "/pacify/precache-manifest.6f79be4faeaa9c2be31153c12bfd8a53.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timepalace/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pacify/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
